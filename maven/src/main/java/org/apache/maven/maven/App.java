00405172979bfba4a291479213ab43824e31aa20
PaulineF <flambard.pauline@laposte.net>
2016-01-06 18:13:39 +0100

    2 commit

---
+++ maven/src/main/java/org/apache/maven/maven/App.java
@@ -0,0 +1,20 @@
+package org.apache.maven.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  /**
+   * main
+   * @param args test
+   */
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+    if (true){
+    	System.out.print(true);
+    }else{
+    	System.out.print("ok");
+    }
+  }
+}
